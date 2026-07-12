cca2d20c284e622f6fc6556a810e88fff23c7e55
Anastasiia_Aghfir <anastasiia.aghfir@gmail.com>
2026-07-12 23:06:07 +0200

    hw-04-fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
@@ -9,18 +9,25 @@ import MovieModal from '../MovieModal/MovieModal';
 import fetchMovies from '../../services/movieService';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import Pagination from '../Pagination/Pagination';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function App() {
   const [query, setQuery] = useState('');
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ['movies', query, currentPage],
     queryFn: () => fetchMovies(query, currentPage),
     enabled: query !== '',
     placeholderData: keepPreviousData,
   });
+
+  useEffect(() => {
+  if (isSuccess && data?.results.length === 0) {
+    toast.error("No movies found for your request.");
+  }
+}, [isSuccess, data]);
 
   const handleSearch = async (query: string) => {
     setQuery(query);
@@ -30,6 +37,7 @@ export default function App() {
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
+      <Toaster />
       {isLoading && <Loader />}
       {!isError && !isLoading && data?.results && (
         <MovieGrid onSelect={setSelectedMovie} movies={data.results} />
